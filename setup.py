ad3259427ac21991ab5bd9c8441248c44d85064c
kevinksyTRD <54361535+kevinksyTRD@users.noreply.github.com>
2020-08-20 13:54:25 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='pyCoSim',
     version='0.1.0',
     description='Python library running co-simulation using cosim-cli from Open Simulation Platform',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kevinTRD/pycosim",
     author='Kevin Koosup Yum',
     author_email='kevinkoosup.yum@sintef.no',
     license="Apache License 2.0",
